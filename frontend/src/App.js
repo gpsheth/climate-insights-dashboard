1301adf1a0139f02fbe5b924417f2a75454d8566
Gaurav Sheth <gpsheth@ncsu.edu>
2025-06-16 03:15:17 -0700

    add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -77,7 +77,7 @@ function App() {
             }
         }
 
-        const res = await fetch('http://localhost:5000/weather', {
+        const res = await fetch('http://10.220.4.157:5000/weather', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ location, startDate, endDate, hourlyVars, selectedCharts }),
