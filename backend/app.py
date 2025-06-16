8f639216470a657a589e5629eb8591ad3f10c1f6
Gaurav Sheth <gpsheth@ncsu.edu>
2025-06-16 03:02:28 -0700

    app.py

--- backend/app.py
+++ backend/app.py
@@ -44,4 +44,4 @@ def get_cached():
         return jsonify({"error": "No cached data found"}), 404
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', , port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
